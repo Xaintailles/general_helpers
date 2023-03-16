78ab87027612eccef3d76cb51d2dbfa797659d14
Xaintailles <calixte.allier@gmail.com>
2023-03-16 08:16:04 +0100

    first commit

--- list_files_per_folder.py
+++ list_files_per_folder.py
@@ -5,3 +5,13 @@ Created on Thu Mar 16 08:03:00 2023
 @author: Calixte
 """
 
+import os
+from os.path import isfile, join
+
+path = r'C:\Users\Calixte\Desktop\GitHub\general_helpers'
+
+for (dirpath, dirnames, filenames) in os.walk(path):
+    print(dirnames)
+    print(filenames)
+    print([os.path.isfile(join(path, f)) for f in os.listdir(path)])
+    break
